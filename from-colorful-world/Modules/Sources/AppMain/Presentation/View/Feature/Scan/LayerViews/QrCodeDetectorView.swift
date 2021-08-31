63bef98646fa53527180b687123b3043fc0daf99
fuziki <hymne.shannon.projects@gmail.com>
2021-09-01 02:23:12 +0900

    make QrCodeDetectorView

---
+++ from-colorful-world/Modules/Sources/AppMain/Presentation/View/Feature/Scan/LayerViews/QrCodeDetectorView.swift
@@ -0,0 +1,24 @@
+//
+//  QrCodeDetectorView.swift
+//  
+//
+//  Created by fuziki on 2021/08/31.
+//
+
+import Combine
+import Foundation
+import SwiftUI
+
+struct QrCodeDetectorView: UIViewRepresentable {
+    private let detector: QrCodeDetectorService
+    init(flip: AnyPublisher<Void, Never>, detected: PassthroughSubject<String, Never>) {
+        detector = DefaultQrCodeDetectorService(flip: flip, detected: detected)
+    }
+
+    func makeUIView(context: Context) -> UIView {
+        return detector.preview
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+    }
+}
