267e6d89fce53980e229919a868ada87ec65cf7d
fuziki <hymne.shannon.projects@gmail.com>
2021-11-26 19:51:41 +0900

    search bundle

--- from-colorful-world/Modules/Sources/Assets/Utils/MyBundleToken.swift
+++ from-colorful-world/Modules/Sources/Assets/Utils/MyBundleToken.swift
@@ -8,22 +8,39 @@
 import Foundation
 
 internal final class MyBundleToken {
-    private static let packageName = "Modules"
-    private static let bundleName: String = {
-        let t = type(of: MyBundleToken())
-        let c = NSStringFromClass(t)
-        let m = c.components(separatedBy: ".").first!
-        return "\(packageName)_\(m)"
+    private static let previewBundlePath: URL = {
+        let typeName = type(of: MyBundleToken())
+        // "\(ModuleName).MyBundleToken"
+        let nsClassString = NSStringFromClass(typeName)
+        guard let bundleName = nsClassString.components(separatedBy: ".").first else {
+            fatalError("failed get bundle name: \(nsClassString)")
+        }
+
+        let searchUrl: URL = Bundle(for: MyBundleToken.self)
+            .bundleURL
+            .deletingLastPathComponent()
+            .deletingLastPathComponent()
+        let list: [URL]
+        do {
+            list = try FileManager.default.contentsOfDirectory(at: searchUrl, includingPropertiesForKeys: nil)
+        } catch let error {
+            fatalError("failed get contents: \(searchUrl)")
+        }
+        let firstMatch: URL? = list.first { (url: URL) in
+            let name = url.lastPathComponent
+            return name.contains("_\(bundleName).bundle")
+        }
+        guard let res = firstMatch else {
+            let jointed = list.map { $0.relativeString }
+                .joined(separator: "\n")
+            fatalError("no url contains: _\(bundleName).bundle\n\(jointed)")
+        }
+        return res
     }()
     static let bundle: Bundle = {
         #if SWIFT_PACKAGE
         if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
-            let url = Bundle(for: MyBundleToken.self)
-                .bundleURL
-                .deletingLastPathComponent()
-                .deletingLastPathComponent()
-                .appendingPathComponent(bundleName + ".bundle")
-            return Bundle(url: url)!
+            return Bundle(url: previewBundlePath)!
         } else {
             return Bundle.module
         }
