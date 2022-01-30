e2d2526775509f5b79a0361dc0b5f7fa44414b94
fuziki <hymne.shannon.projects@gmail.com>
2022-01-30 22:31:40 +0900

    add MainViewModelTest test case

--- from-colorful-world/Modules/Tests/AppMainTests/Feature/Presentation/ViewModel/Feature/Main/MainViewModelTest.swift
+++ from-colorful-world/Modules/Tests/AppMainTests/Feature/Presentation/ViewModel/Feature/Main/MainViewModelTest.swift
@@ -42,4 +42,66 @@ final class MainViewModelTest: XCTestCase {
             .onNext(now.advanced(by: .seconds(1)), true),
         ])
     }
+
+    func testReadHasNew() {
+        var cancellables: Set<AnyCancellable> = []
+
+        let latestInfomationDate = Date(timeIntervalSince1970: 1460214000)
+        let usecase = MainViewUseCaseMock()
+        usecase.fetchLatestInfomationDateHandler = { (_: String) -> AnyPublisher<Date, Error> in
+            return .just(output: latestInfomationDate)
+        }
+        usecase.latestReadInfomationDate = latestInfomationDate.advanced(by: -3600)
+        let viewModel = MainViewModel(usecase: usecase)
+
+        let scheduler = DispatchQueue.test
+        let showBadgeSubject = TestEventRecoder(outputType: Bool.self, testScheduler: scheduler)
+        let now = scheduler.now
+        
+        scheduler.schedule(after: now.advanced(by: .seconds(1))) {
+            viewModel.onAppear()
+        }
+        
+        viewModel.showBadge.sink { (showBadge: Bool) in
+            showBadgeSubject.send(showBadge)
+        }.store(in: &cancellables)
+
+        scheduler.advance(by: .seconds(3))
+
+        XCTAssertEqual(showBadgeSubject.eventRecorded, lineAssertReferences: [
+            .onNext(now, false),
+            .onNext(now.advanced(by: .seconds(1)), true),
+        ])
+    }
+
+    func testReadHasNoNew() {
+        var cancellables: Set<AnyCancellable> = []
+
+        let latestInfomationDate = Date(timeIntervalSince1970: 1460214000)
+        let usecase = MainViewUseCaseMock()
+        usecase.fetchLatestInfomationDateHandler = { (_: String) -> AnyPublisher<Date, Error> in
+            return .just(output: latestInfomationDate)
+        }
+        usecase.latestReadInfomationDate = latestInfomationDate.advanced(by: 3600)
+        let viewModel = MainViewModel(usecase: usecase)
+
+        let scheduler = DispatchQueue.test
+        let showBadgeSubject = TestEventRecoder(outputType: Bool.self, testScheduler: scheduler)
+        let now = scheduler.now
+        
+        scheduler.schedule(after: now.advanced(by: .seconds(1))) {
+            viewModel.onAppear()
+        }
+        
+        viewModel.showBadge.sink { (showBadge: Bool) in
+            showBadgeSubject.send(showBadge)
+        }.store(in: &cancellables)
+
+        scheduler.advance(by: .seconds(3))
+
+        XCTAssertEqual(showBadgeSubject.eventRecorded, lineAssertReferences: [
+            .onNext(now, false),
+            .onNext(now.advanced(by: .seconds(1)), false),
+        ])
+    }
 }
