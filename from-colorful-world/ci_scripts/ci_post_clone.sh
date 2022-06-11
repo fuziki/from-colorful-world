ad37443995d0febb7c5f128d3226bdd66eb9c7a6
fuziki <hymne.shannon.projects@gmail.com>
2022-06-11 23:36:37 +0900

    update ci_post_clone.sh

--- from-colorful-world/ci_scripts/ci_post_clone.sh
+++ from-colorful-world/ci_scripts/ci_post_clone.sh
@@ -1,13 +1,3 @@
-echo "show default path"
-pwd
-
-cd ${CI_PROJECT_FILE_PATH}
-echo "show CI_PROJECT_FILE_PATH"
-pwd
-
-cd ${CI_WORKSPACE}
-echo "show CI_WORKSPACE"
-pwd
 
 echo "make install"
-make install
+make install -C ${CI_WORKSPACE}
