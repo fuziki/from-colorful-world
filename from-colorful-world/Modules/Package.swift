1a9aee619ec1f77f6c50e6240108bd3dbf4f87a5
fuziki <hymne.shannon.projects@gmail.com>
2022-10-08 11:01:49 +0900

    update combine-schedulers

--- from-colorful-world/Modules/Package.swift
+++ from-colorful-world/Modules/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/pointfreeco/combine-schedulers.git", from: "0.5.3"),
+        .package(url: "https://github.com/pointfreeco/combine-schedulers.git", from: "0.8.0"),
         .package(url: "https://github.com/huri000/SwiftEntryKit.git", from: "2.0.0"),
     ],
     targets: [
